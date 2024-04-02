d347ea50c8fc339108637f751dd9ba1fba049ccd
CAPSALC <lunacortesantonio@gmail.com>
2024-04-02 13:44:39 -0500

    Se agrega boostrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import bootstrap from './main.server';
+
+// IMPORTAR bootstrap
+
+// import * as $ from 'jquery';
+
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
+import '../node_modules/@popperjs/core/dist/umd/popper.min.js'
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
